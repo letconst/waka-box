c1955175a1d466936b615d625eef18f283898511
Takeru Endo <67447234+letconst@users.noreply.github.com>
2021-12-05 18:05:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function updateGist(stats) {
     const { name, percent, text: time } = data;
 
     const line = [
-      trimRightStr(name, 10).padEnd(10),
+      trimRightStr(name, 13).padEnd(13),
       time.padEnd(14),
       generateBarChart(percent, 21),
       String(percent.toFixed(1)).padStart(5) + "%"
